2fad963e1254153fc331539bf576e414870e66b0
bishaldan <124037829+bishaldan@users.noreply.github.com>
2023-12-06 00:11:07 +0545

    First1

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+import util
+class App:
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.main_window.geometry("1200x520+350+100")
+        self.login_button_main_window = util.get_button(self.main_window, 'login', 'RED', self.login)
+        self.login_button_main_window.place(x=750, y=300)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray', self.register_new_user, fg='black')
+        self.register_new_user_button_main_window.place(x=750, y=400)
+
+        self.webcam_label = util.get_img_label(self.main_window)
+        self.webcam_label.place(x=10, y=0, width=700, height=500)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        self.add_webcam(self.webcam_label)
+
+    def add_webcam(self, label):
+        if 'cap'  not in self.__dict__:
+            self.cap = cv2.VideoCapture(0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        self._label = label
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        self.process_webcam()
+
+    def process_webcam(self):
+        ret, frame = self.cap.read()
+
+        self.most_recent_capture_arr = frame
+
+        img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
+        self.most_recent_capture_pill = Image.fromarray(img_)
+
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pill)
+
+        self._label.imgtk = imgtk
+        self._label.configure(image=imgtk)
+
+        self._label.after(20, self.process_webcam)
+
+    def login(self):
+        pass
+    def register_new_user(self):
+        self.register_new_user_window = tk.Toplevel(self.main_window)
+        self.register_new_user_window.geometry("1200x520+370+120")
+
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+        self.accept_button_register_new_user_window.place(x=750, y=300)
+
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try again', 'RED', self.accept_register_new_user)
+        self.try_again_button_register_new_user_window.place(x=750, y=400)
+
+        self.capture_label = util.get_img_label(self.register_new_user_window)
+        self.capture_label.place(x=10, y=0, width=700, height=500)
+
+        self.add_img_to_label(self.capture_label)
+
+
+    def add_img_to_label(self, label):
+        pass
+
+    def start(self):
+        self.main_window.mainloop()
+
+    def accept_register_new_user(self):
+        pass
+    # def try_again_register_new_user(self):
+    #     pass
+
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
